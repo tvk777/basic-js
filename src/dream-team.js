92ed9e015678609e75c71475fc4c6d0204a47856
tvk777 <klimenko.tv7@gmail.com>
2020-10-14 18:00:19 +0300

    feat: resolve task createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  return members && members.length > 0
+    ? members.map(name =>
+      typeof name === 'string' && name.length > 0 && name !== ' '
+        ? name.trim().charAt(0).toUpperCase()
+        : ''
+    ).sort().join('')
+    : false;
 };
