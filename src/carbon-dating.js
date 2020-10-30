1f71339b9e0e5e74a55d52ec8d489858774e55e2
tvk777 <klimenko.tv7@gmail.com>
2020-10-30 21:45:29 +0200

    feat: resolve what-seson and carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  //throw new CustomError('Not implemented');
+  let concentration = 0.693 / HALF_LIFE_PERIOD;
+  let sampleAge = Math.log(MODERN_ACTIVITY / sampleActivity) / concentration;
+  if (!sampleActivity || sampleActivity == 0 || isNaN) {
+    return false;
+  } else return Math.ceil(sampleAge);
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,13 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
+Date.prototype.isValid = function () {
+  return this.getTime() === this.getTime();
+}; 
+
 module.exports = function getSeason(date) {
   if (!date) return 'Unable to determine the time of year!';
-  //if (!(date instanceof Date)) return 'THROWN';
+  if (!date.isValid()) return null;
   try {
-    date instanceof Date;
+   date instanceof Date
   } catch (err) {
     return 'THROWN';
   }
+
   const month = date.getMonth();
   if (month === 11 || month === 0 || month === 1) return 'winter';
   if (month === 2 || month === 3 || month === 4) return 'spring';
