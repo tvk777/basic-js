12326711ba801b492033c62b82aeb2f94d503343
tvk777 <37848270+tvk777@users.noreply.github.com>
2020-10-31 17:51:41 +0200

    feat: start to resolve transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,26 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform(arr) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let result = arr;
+  result.map((r, i) => {
+    switch (r) {
+      case '--discard-next':
+        result.splice(i, 1);
+        result.splice(i, 1);
+        break;
+      case '--discard-prev':
+        result.splice(i, 1);
+        result.splice(i - 1, 1);
+        break;
+      case '--double-next':
+        result.splice(i, 1, result[i + 1]);
+        break;
+      case '--double-prev':
+        result.splice(i, 1, result[i - 1]);
+        break;
+    }
+  })
+  return result;
 };
